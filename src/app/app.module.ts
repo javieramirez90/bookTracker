6f5b0918d854b86ddbd825d43872be2edb3ee45a
javieramirez90 <javier.ramirez@aiesec.net>
2018-10-01 22:38:15 -0500

    Http interceptors practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { AddReaderComponent } from './add-reader/add-reader.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { EditReaderComponent } from './edit-reader/edit-reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    AddReaderComponent,
+    DashboardComponent,
+    EditBookComponent,
+    EditReaderComponent
   ],
   imports: [
     BrowserModule
